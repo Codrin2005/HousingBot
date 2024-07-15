263f435327093bf292e82e63cdeee1386e1c92bd
Codrin Radetchi <Radetchi@student.tudelft.nl>
2024-07-15 23:37:02 +0300

    works with put call

--- Client/mainPage.js
+++ Client/mainPage.js
@@ -1,3 +1,24 @@
-document.getElementById('submitButton').addEventListener('click', function() {
-    console.log('Button was clicked!');
+document.getElementById('submitButton').addEventListener('click', async () => {
+    const url = 'http://localhost:3000/api/email';
+    const data = String(document.getElementById('email').value);
+
+    try {
+        console.log(data);
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ data: data })
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const responseData = await response.json();
+        console.log('Response:', responseData);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 });
--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/api/data', (req, res) => {
-    res.json({ message: 'Hello from the server!' });
+const fs = require('fs');
+
+
+app.put('/api/email', (req, res) => {
+    const mail = req.body.data;
+    console.log(typeof mail);
+    console.log(req.body);
+    fs.writeFile('./Server/emailList.txt', String(mail), (err) => {
+        if (err) throw err;
+        console.log('Email has been written to emailList.txt');
+    });
+    res.status(200).json({ message: 'Email has been written successfully' });
 });
 
 app.listen(port, () => {
