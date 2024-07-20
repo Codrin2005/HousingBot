216fbe2510940400a902ab0c3e24766eb405dcda
Codrin Radetchi <Radetchi@student.tudelft.nl>
2024-07-20 14:15:57 +0300

    typo

--- Server/botRunner.js
+++ Server/botRunner.js
@@ -1,5 +1,5 @@
 const cp = require("child_process");
 const psData = cp.spawn
-("powershell -executionpolicy bypass  ./Plaza_Script.ps1", [], {
+("powershell -executionpolicy bypass  ./Bots/Plaza/Plaza_Script.ps1", [], {
     shell: "powershell.exe",
 });
