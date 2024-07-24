e102656833f589f5ce0fc2afe8d46d41f8ede99c
Codrin Radetchi <Radetchi@student.tudelft.nl>
2024-07-24 11:23:46 +0300

    managed to send email, now I must send with right body

--- Server/mailService.js
+++ Server/mailService.js
@@ -1,13 +1,48 @@
-function sendMails(body){
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: "rcodrin13@gmail.com",
-        Password: "pykj ixbb mtms wovw",
-        To: 'rcodrin13@gmail.com',
-        From: "rcodrin13@gmail.com",
-        Subject: "Sending Email using JavaScript",
-        Body: "This is a test email sent using JavaScript!",
-    })
+const nodemailer = require('nodemailer');
+const fs = require('fs').promises;
+
+async function readFile() {
+    try {
+        const data = await fs.readFile('./Server/emailList.txt', 'utf8');
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function sendMails(body){
+    let receivers = await readFile();
+    
+
+    // Create a transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: "rcodrin13@gmail.com", // your email
+            pass: "pykj ixbb mtms wovw", // your email password
+        },
+    });
+
+    // Setup email data
+    let mailOptions = {
+        from: '"HousingBot" <rcodrin13@gmail.com>', // sender address
+        to: receivers, // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+    };
+
+    // Send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    });
+
 }
 
 module.exports = {
