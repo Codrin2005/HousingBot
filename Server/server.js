361f224f730462f138cf8bcb8bd229f8fda31638
Codrin Radetchi <Radetchi@student.tudelft.nl>
2024-08-31 13:39:56 +0300

    reconfigurated the api

--- Server/server.js
+++ Server/server.js
@@ -7,17 +7,22 @@ const cors = require('cors');
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'null'
+}));
 
 const fs = require('fs');
 
-app.put('/api/email', (req, res) => {
-    const mail = req.body.data;
-    fs.appendFile('./Server/emailList.txt', String(mail) + '\n', (err) => {
+let users = [];
+
+app.put('/api/add-user', (req, res) => {
+    const user = req.body;
+    users.push(user);
+    fs.writeFile('./Server/emailList.txt', JSON.stringify(users), (err) => {
         if (err) throw err;
-        console.log('"' + String(mail) + '" has been written to emailList.txt');
-    });
-    res.status(200).json({ message: 'Email has been written successfully' });
+        console.log("The following user has been added to the file: \n" + JSON.stringify(user));
+    })
+    res.status(200).json({ message: 'User has been saved successfully'});
 });
 
 app.post('/api/create-stripe-payment', async (req, res) => {
@@ -45,4 +50,17 @@ app.post('/api/create-stripe-payment', async (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
+    fs.readFile('./Server/emailList.txt', 'utf-8', (err, data) => {
+        if (err) {
+            console.error('Error reading users file:', err);
+            users = [];
+        } else {
+            try {
+                users = JSON.parse(data);
+            } catch (e) {
+                console.error('Error parsing JSON:', e);
+                users = [];
+            }
+        }
+    });
 });
